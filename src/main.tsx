c8d1171560c4c36c381528b5e2da019935496133
refractglobal <thomas@vanmelsem.com>
2023-04-08 22:19:00 +0200

    StrictMode Off

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,5 @@ import './index.css'
 import ThreeJS from './modules/Three'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <ThreeJS />
-  </React.StrictMode>,
 )
